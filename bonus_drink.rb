3e4d3fb253e60610bbbe80291094ba79b147cd0e
Christian Albert <c_albert@yahoo.com>
2015-11-23 14:26:49 +0900

    added 2 tests, business logic, and solution for 100
    2 test files
    - 'bonus_drink_spec.rb' test file mimics the syntax of the example provided
    - 'bonus_drink_spec.rb' test file produces easier msgs for test failures
    
    Business Logic
    - 'bonus_drink.rb'
    
    Solution for 100
    - 'solve_for_100.rb'

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,14 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    bought = amount
+    free = 0
+    i = 1
+    while i <= bought + free
+      if i%3 == 0
+        free += 1
+      end
+      i += 1
+    end
+    return bought + free
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
+puts "\nRunning tests on BonusDrink.total_count_for()"
+
 describe BonusDrink do
+  specify { expect(BonusDrink.total_count_for(0).integer?).to be true }
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
+  specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
 end
